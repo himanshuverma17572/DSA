77d76257ace84bd372dfb425b6c50c4b91a411fd
ravishanker32 <ravi.shankercse@gmail.com>
2022-09-12 20:50:03 +0530

    Added code for RichestCustomer

---
+++ src/com/RichestCustomer.java
@@ -0,0 +1,23 @@
+package com;
+
+public class RichestCustomer {
+    public static void main(String[] args) {
+        int[][] allCustomersAccounts = {{5, 2, 3}, {4, 6, 2}, {12, 3, 5}};
+        System.out.println(calculateRichestCustomer(allCustomersAccounts));
+    }
+
+    static int calculateRichestCustomer(int[][] allCustomersAccounts) {
+        int wealthOfRicherstCustomer = Integer.MIN_VALUE;
+        for (int[] customerAccounts : allCustomersAccounts) {
+            int totalWealthOfACustomer = 0;
+            for (int amountInEachBank : customerAccounts) {
+                totalWealthOfACustomer += amountInEachBank;
+            }
+            if (totalWealthOfACustomer > wealthOfRicherstCustomer) {
+                wealthOfRicherstCustomer = totalWealthOfACustomer;
+            }
+        }
+        return wealthOfRicherstCustomer;
+    }
+
+}
