e27349094ffb653057f190db26a18211b018e0a0
ravishanker32 <ravi.shankercse@gmail.com>
2022-09-15 15:22:01 +0530

    Updated code for BinarySearch

--- src/com/binarySearch/BinarySearch.java
+++ src/com/binarySearch/BinarySearch.java
@@ -1,16 +1,17 @@
 package com.binarySearch;
 
-public class BinarySearchDemo {
+import java.util.Arrays;
+
+public class BinarySearch {
 
     public static void main(String[] args) {
 //        int[] sortedArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
         int[] sortedArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16};
-        System.out.println(findIndexPositionUsingBinarySearch(sortedArray, -5));
-        System.out.println(findIndexPositionUsingBinarySearch(sortedArray, 0));
-        System.out.println(findIndexPositionUsingBinarySearch(sortedArray, 3));
-        System.out.println(findIndexPositionUsingBinarySearch(sortedArray, 10));
-        System.out.println(findIndexPositionUsingBinarySearch(sortedArray, 13));
-        System.out.println(findIndexPositionUsingBinarySearch(sortedArray, 19));
+
+        for (int elementToSearch : Arrays.asList(-5, 0, 3, 10, 13, 19)) {
+            int index = findIndexPositionUsingBinarySearch(sortedArray, elementToSearch);
+            System.out.println(-1 != index ? String.format("%s exists at index:%s", elementToSearch, index) : String.format("%s does not exists in the given array", elementToSearch));
+        }
     }
 
     static int findIndexPositionUsingBinarySearch(int[] sortedArray, int target) {
@@ -24,15 +25,15 @@ public class BinarySearchDemo {
             while (true) {
                 indexPositionOfMidElement = (indexPositionOfStartElement + indexPositionOfEndElement) / 2;
                 midElement = sortedArray[indexPositionOfMidElement];
-                if (target < midElement) {
+                if (indexPositionOfStartElement == indexPositionOfEndElement && target != midElement) {
+                    break;
+                } else if (target < midElement) {
                     indexPositionOfEndElement = indexPositionOfMidElement - 1;
                 } else if (target == midElement) {
                     indexPositionOfTargetElement = indexPositionOfMidElement;
                     break;
                 } else if (target > midElement) {
                     indexPositionOfStartElement = indexPositionOfMidElement + 1;
-                } else if (indexPositionOfStartElement == indexPositionOfEndElement) {
-                    break;
                 }
             }
         }
