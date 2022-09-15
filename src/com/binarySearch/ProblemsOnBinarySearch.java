9a64206f879f9ce616ec388e4842e167947bc4df
ravishanker32 <ravi.shankercse@gmail.com>
2022-09-15 19:45:38 +0530

    Update ProblemsOnBinarySearch.java

--- src/com/binarySearch/ProblemsOnBinarySearch.java
+++ src/com/binarySearch/ProblemsOnBinarySearch.java
@@ -5,31 +5,43 @@ import java.util.Arrays;
 public class ProblemsOnBinarySearch {
 
     public static void main(String[] args) {
-        int[] ascendingSortedArray = {-3, -2, -1, 0, 1, 3, 5, 6, 7, 11, 12, 14, 16};
+        int[] ascendingSortedIntArray = {-3, -2, -1, 0, 1, 3, 5, 6, 7, 11, 12, 14, 16};
+        // Finding ceiling of a number
         for (int target : Arrays.asList(-4, -5, -1, 0, 3, 9, 10, 19)) {
-            int ceilingOfNumber = ceilingOfNumber(ascendingSortedArray, target);
-            System.out.println(String.format("Ceiling of %s in Array:%s = %s", target, Arrays.toString(ascendingSortedArray), ceilingOfNumber != Integer.MIN_VALUE ? ceilingOfNumber : "Does not exists"));
+            int indexOfCeilingOfNumber = ceilingOfNumber(ascendingSortedIntArray, target, true);
+            System.out.println(String.format("Ceiling of %s in Array:%s = %s", target, Arrays.toString(ascendingSortedIntArray), indexOfCeilingOfNumber != -1 ? ascendingSortedIntArray[indexOfCeilingOfNumber] : "Does not exists"));
+        }
+        System.out.println("=================================================================================");
+        // Finding floor of a number
+        for (int target : Arrays.asList(-4, -5, -1, 0, 3, 9, 10, 19)) {
+            int indexOfCeilingOfNumber = ceilingOfNumber(ascendingSortedIntArray, target, false);
+            System.out.println(String.format("Floor of %s in Array:%s = %s", target, Arrays.toString(ascendingSortedIntArray), indexOfCeilingOfNumber != -1 ? ascendingSortedIntArray[indexOfCeilingOfNumber] : "Does not exists"));
+        }
+        System.out.println("=================================================================================");
+        // Finding ceiling of a char
+        char[] ascendingSortedCharArray = {'H', 'c', 'f', 'g', 'k'};
+        for (char target : Arrays.asList('a', 'c', 'm', 'A')) {
+            int indexOfCeilingOfCharacter = ceilingOfCharacter(ascendingSortedCharArray, target, true);
+            System.out.println(String.format("Ceiling of %s in Array:%s = %s", target, Arrays.toString(ascendingSortedCharArray), indexOfCeilingOfCharacter != -1 ? ascendingSortedCharArray[indexOfCeilingOfCharacter] : "Does not exists"));
         }
     }
 
-    static int ceilingOfNumber(int[] arr, int target) {
+    static int ceilingOfNumber(int[] arr, int target, boolean findCeiling) {
         int indexOfStartElement = 0;
         int indexOfEndElement = arr.length - 1;
         int indexOfMidElement;
         int midElement;
         boolean isAscneding = arr[indexOfStartElement] < arr[indexOfEndElement];
 
-        if (target < arr[indexOfStartElement])
-            return arr[indexOfStartElement];
-        else if (arr[indexOfEndElement] < target)
-            return Integer.MIN_VALUE;
+        if (findCeiling && arr[indexOfEndElement] < target || !findCeiling && target < arr[indexOfStartElement])
+            return -1;
 
         while (indexOfStartElement <= indexOfEndElement) {
             indexOfMidElement = indexOfStartElement + (indexOfEndElement - indexOfStartElement) / 2;
             midElement = arr[indexOfMidElement];
 
             if (target == midElement)
-                return midElement;
+                return indexOfMidElement;
 
             if (isAscneding) {
                 if (target < midElement)
@@ -43,7 +55,39 @@ public class ProblemsOnBinarySearch {
                     indexOfStartElement = indexOfMidElement + 1;
             }
         }
-
-        return arr[indexOfStartElement];
+        return findCeiling ? indexOfStartElement : indexOfEndElement;
     }
+
+    static int ceilingOfCharacter(char[] arr, char target, boolean findCeiling) {
+        int indexOfStartElement = 0;
+        int indexOfEndElement = arr.length - 1;
+        int indexOfMidElement;
+        char midElement;
+        boolean isAscneding = arr[indexOfStartElement] < arr[indexOfEndElement];
+
+        if (findCeiling && arr[indexOfEndElement] < target || !findCeiling && target < arr[indexOfStartElement])
+            return -1;
+
+        while (indexOfStartElement <= indexOfEndElement) {
+            indexOfMidElement = indexOfStartElement + (indexOfEndElement - indexOfStartElement) / 2;
+            midElement = arr[indexOfMidElement];
+
+            if (target == midElement)
+                return indexOfMidElement;
+
+            if (isAscneding) {
+                if (target < midElement)
+                    indexOfEndElement = indexOfMidElement - 1;
+                else if (midElement < target)
+                    indexOfStartElement = indexOfMidElement + 1;
+            } else {
+                if (target > midElement)
+                    indexOfEndElement = indexOfMidElement - 1;
+                else if (midElement > target)
+                    indexOfStartElement = indexOfMidElement + 1;
+            }
+        }
+        return findCeiling ? indexOfStartElement : indexOfEndElement;
+    }
+
 }
